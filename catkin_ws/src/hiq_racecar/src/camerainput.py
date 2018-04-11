a00a3bc3d3435f960fdfe657d3779cdb4d2ae993
Jetson TX2 <jetson@tx2.com>
2018-04-11 16:43:16 +0000

    Fixing the autonomous car
    Almost self-driving car

--- catkin_ws/src/hiq_racecar/src/camerainput.py
+++ catkin_ws/src/hiq_racecar/src/camerainput.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
 import rospy
+import roslib
 import sys
 import cv2
 import numpy as np
-from std_msgs.msg import Float32MultiArray as Array
+from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+
 topic = "raw_camera" 
 
 
 def camera_publish():
-	publisher = rospy.Publisher(topic, String, queue_size= 10)
+	publisher = rospy.Publisher(topic, Image, queue_size= 10)
         print cv2.__version__
+	bridge = CvBridge()
 	rospy.init_node('camera', anonymous=True)
         rate = rospy.Rate(10)
 	print('Hello from print')
@@ -19,18 +23,20 @@ def camera_publish():
 	cap = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")	
 	#cap = cv2.VideoCapture("/dev/video0")
 	rospy.loginfo(cap)
+	rate.sleep()
 	print(cap)
 	if not cap.isOpened():
 		rospy.loginfo("Failed to open cap")
-		print("Failed to open cap")
+	print("Failed to open cap")
 
 	while not rospy.is_shutdown():
 		ret_val, camera_value = cap.read()
-		rospy.loginfo('New Message')
-		rospy.loginfo(camera_value)
-		
+		#rospy.loginfo('New Message')
+		#rospy.loginfo(camera_value)
+		#print(np.squeeze(np.asarray(camera_value)))	
+
 		if ret_val:
-			publisher.publish(np.squeeze(np.asarray(camera_value)))
+			publisher.publish(bridge.cv2_to_imgmsg(camera_value, "bgr8"))
 		rate.sleep()
 
 if __name__ == '__main__':

de3e5eef1cea30a06fe44bb119b67d3eb9eadfec
Adam Andersson <adam.ottvar@gmail.com>
2018-04-25 19:38:08 +0200

    Made init imu function

--- catkin_ws/src/hiq_racecar/src/imu_pub.py
+++ catkin_ws/src/hiq_racecar/src/imu_pub.py
@@ -9,11 +9,43 @@ import os.path
 import time
 import math
 
-#SETTINGS_FILE = "RTIMULib"
-
 SETTINGS_FILE = "../include/hiq_racecar/RTIMULib"
 
 #Add supportfunctions here to read the values from the hardware
+def init_imu():
+    print("Using settings file " + SETTINGS_FILE + ".ini")
+    
+    if not os.path.exists(SETTINGS_FILE + ".ini"):
+        print("Settings file does not exist, will be created")
+
+    settings = RTIMU.Settings(SETTINGS_FILE)
+    imu_instance = RTIMU.RTIMU(settings)
+
+    print("IMU Name: " + imu_instance.IMUName())
+
+    if (not imu_instance.IMUInit()):
+        print("IMU Init Failed")
+        sys.exit(1)
+    else:
+        print("IMU Init Succeeded")
+
+    # this is a good time to set any fusion parameters
+
+    imu_instance.setSlerpPower(0.02)
+    imu_instance.setGyroEnable(True)
+    imu_instance.setAccelEnable(True)
+    imu_instance.setCompassEnable(True)
+
+    poll_interval = imu_instance.IMUGetPollInterval()
+    print("Recommended Poll Interval: %dmS\n" % poll_interval)   
+    
+    acc_x, acc_y, acc_z = imu_instance.getAccel()
+    gyro_x, gyro_y, gyro_z = imu_instance.getGyro()
+    mag_x, mag_y, mag_z = imu_instance.getCompass()
+
+    rospy.loginfo('Initiated IMU')
+    
+    return imu_instance
 
 def imu_publish():
     imu_raw = Imu()
@@ -22,35 +54,8 @@ def imu_publish():
     pub_mag = rospy.Publisher('mag_data', MagneticField, queue_size = 1)
     rospy.init_node('imu_pub')
     rate = rospy.Rate(10) # 1 Hz
-    print("Using settings file " + SETTINGS_FILE + ".ini")
     
-    if not os.path.exists(SETTINGS_FILE + ".ini"):
-        print("Settings file does not exist, will be created")
-
-    s = RTIMU.Settings(SETTINGS_FILE)
-    imu = RTIMU.RTIMU(s)
-
-    print("IMU Name: " + imu.IMUName())
-
-    if (not imu.IMUInit()):
-        print("IMU Init Failed")
-        sys.exit(1)
-    else:
-        print("IMU Init Succeeded")
-
-    # this is a good time to set any fusion parameters
-
-    imu.setSlerpPower(0.02)
-    imu.setGyroEnable(True)
-    imu.setAccelEnable(True)
-    imu.setCompassEnable(True)
-
-    poll_interval = imu.IMUGetPollInterval()
-    print("Recommended Poll Interval: %dmS\n" % poll_interval)   
-    
-    acc_x, acc_y, acc_z = imu.getAccel()
-    gyro_x, gyro_y, gyro_z = imu.getGyro()
-    mag_x, mag_y, mag_z = imu.getCompass()
+    imu = init_imu()
 
     while not rospy.is_shutdown():        
         # Get imu data
@@ -58,10 +63,11 @@ def imu_publish():
 	    acc_x, acc_y, acc_z = imu.getAccel()
             gyro_x, gyro_y, gyro_z = imu.getGyro()
             mag_x, mag_y, mag_z = imu.getCompass()
+	    rospy.loginfo('Read imu data')
         	
         imu_raw.linear_acceleration.x = acc_x
         imu_raw.linear_acceleration.y = acc_y
-	imu_raw.linear_acceleration.z = acc_z
+		imu_raw.linear_acceleration.z = acc_z
 
         imu_raw.angular_velocity.x = gyro_x
         imu_raw.angular_velocity.y = gyro_y
@@ -72,7 +78,7 @@ def imu_publish():
         mag_raw.magnetic_field.z = mag_z
 
         #Use loginfo for loggin data in order to find errors
-        rospy.loginfo('Read imu data')
+        #rospy.loginfo('Read imu data')
         #Use pub that was created above to publish the data list, string etc.
         pub_imu.publish(imu_raw)
         pub_mag.publish(mag_raw)

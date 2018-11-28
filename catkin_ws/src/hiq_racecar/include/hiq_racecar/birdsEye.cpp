6e6fa4cda35a559719319ef2abb5a4e1c33655b8
Albin <albin.lorentzson@me.com>
2018-11-28 17:52:55 +0100

    Added birdseye

--- catkin_ws/src/hiq_racecar/include/hiq_racecar/birdsEye.cpp
+++ catkin_ws/src/hiq_racecar/include/hiq_racecar/birdsEye.cpp
@@ -1,4 +1,90 @@
-#include "birdsEye.h"
+#include "BirdsEye.h"
+#include <boost/format.hpp>
 
-BirdsEye::BirdsEye() {
+using namespace cv;
+
+BirdsEye::BirdsEye(Point2f sourcepoints_l[], Point2f destpoints_l[], Mat camMatrix, double distCoef)
+{
+    for (int i = 0; i < 4; i++) {
+        this->srcPoints[i] = sourcepoints_l[i];
+        this->destPoints[i] = destpoints_l[i];
+    }
+
+    this->camMatrix = camMatrix;
+    this->distCoef = distCoef;
+
+    this->warpMatrix = getPerspectiveTransform(this->srcPoints, this->destPoints);
+    this->invWarpMatrix = getPerspectiveTransform(this->srcPoints, this->destPoints);
 }
+
+BirdsEye::~BirdsEye(){
+}
+
+void BirdsEye::undistort_image(Mat* rawImage, Mat* undistoredImage){
+
+    undistort(*rawImage, *undistoredImage, camMatrix, distCoef);
+}
+
+void BirdsEye::skyView(const Mat groundImage, Mat* outputImage){
+
+    //TODO(albin): uncomment this when we have a functional callibration matrix
+    //Mat undistorted_image;
+    //undistort(&ground_image, undistored_image);
+    Size shape= groundImage.size();
+    warpPerspective(groundImage, *outputImage, warpMatrix, shape);
+}
+
+void BirdsEye::project(Mat groundImage, Mat* outputImage, double *leftFit, double *rightFit, Scalar *color){
+    /*
+     Function which projects the ground image to a sky image.
+     */
+    cv::Mat skyLane = Mat::zeros(groundImage.size(), groundImage.type());
+
+    int imageHeight = skyLane.size().height;
+    int maxHeight = skyLane.size().height;
+
+    //Linspace :D
+    double ys[imageHeight];
+    double diff = 0;
+    for (int i = 0; i < imageHeight; i++) {
+        diff = diff + (maxHeight/imageHeight);
+        ys[i] = diff;
+    }
+
+    //Quadratic funcition
+    double lxs[imageHeight];
+    double rxs[imageHeight];
+    for (int i = 0; i < imageHeight; i++) {
+        lxs[i] = leftFit[0] * ys[i]*ys[i] + leftFit[1] * ys[i] + leftFit[2];
+        rxs[i] = rightFit[0] * ys[i]*ys[i] + rightFit[1] * ys[i] + rightFit[2];
+    }
+
+    //Convert the polygon points_l to s points_l
+    Point pointsL[1][2*imageHeight];
+
+    for (int i = 0; i < imageHeight; i++) {
+        std::cout << boost::format("values: %s %s %s \n") % ys[i] % lxs[i] % rxs[i];
+        pointsL[0][i]= Point(ys[i], lxs[i]);
+    }
+
+    for (int i = 0; i < imageHeight; i++) {
+    //    std::cout << boost::format("values: %s %s %s \n") % ys[i] % lxs[i] % rxs[i];
+        pointsL[0][i + imageHeight]= Point(ys[i], rxs[i]);
+    }
+
+    const Point* ppt[1] = { pointsL[0]};
+
+    int npt[] = { 2*imageHeight };
+
+    fillPoly(skyLane, ppt, npt, 1, Scalar( 0, 0, 255), 1);
+    Size shape = skyLane.size();
+    Mat lambda = Mat::zeros(skyLane.rows, skyLane.cols, skyLane.type() );
+    lambda = getPerspectiveTransform(this->srcPoints, this->destPoints);
+    Mat warp_image;
+
+    warpPerspective(skyLane, warp_image, lambda, shape);
+    Mat result = Mat::zeros(skyLane.rows, skyLane.cols, groundImage.type());
+    addWeighted(groundImage, 1, warp_image, 0.3, 0, *outputImage);
+}
+
+
--- catkin_ws/src/hiq_racecar/include/hiq_racecar/birdsEye.h
+++ catkin_ws/src/hiq_racecar/include/hiq_racecar/birdsEye.h
@@ -1,20 +1,27 @@
-#ifndef _BIRDS_EYE_H
+#ifdef _BIRDS_EYE_H
 #define _BIRDS_EYE_H
-
-#include <string>
-#include <sstream>
+#endif
 
 #include "opencv2/opencv.hpp"
-#include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/cudaarithm.hpp"
+#include "opencv2/core/mat.inl.hpp"
 
-class BirdsEye {
+
+class BirdsEye
+{
 public:
-    BirdsEye();
 
-private:
+    BirdsEye(cv::Point2f sourcePoints[], cv::Point2f destPoints[], cv::Mat, double);
+    ~BirdsEye();
+	cv::Point2f srcPoints[4];
+	cv::Point2f destPoints[4];
+	cv::Mat camMatrix;
+	cv::Mat warpMatrix;
+	cv::Mat invWarpMatrix;
+    double distCoef;
 
+	void undistort_image(cv::Mat* raw_image, cv::Mat* undistorted_image);
+
+	void skyView(const cv::Mat ground_image, cv::Mat* output_image);
+
+    void project(cv::Mat ground_image, cv::Mat* output_image, double left_fit[3], double right_fit[3], cv::Scalar *color);
 };
-
-#endif // _BIRDS_EYE_H
